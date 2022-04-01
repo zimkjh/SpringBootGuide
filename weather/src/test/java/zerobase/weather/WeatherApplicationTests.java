55bd9377888c8c69fe78b2d55e3b5306f20f42f9
zimkjh <zimkjh@naver.com>
2022-04-01 20:29:17 +0900

    2-3 테스트 코드 실행해보기

--- weather/src/test/java/zerobase/weather/WeatherApplicationTests.java
+++ weather/src/test/java/zerobase/weather/WeatherApplicationTests.java
@@ -3,11 +3,25 @@ package zerobase.weather;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 @SpringBootTest
 class WeatherApplicationTests {
+    @Test
+    void equalTest() {
+        assertEquals(1, 2);
+    }
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void nullTest() {
+        assertNull(null);
+    }
 
+    @Test
+    void trueTest() {
+        assertTrue(1 == 1);
+    }
 }
