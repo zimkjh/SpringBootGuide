89c4c15f831e3df83a0e173b220ff8d82b0df4b7
zimkjh <zimkjh@naver.com>
2022-06-05 18:07:32 +0900

    6-3 트랜잭션 코드에 반영하기

--- weather/src/main/java/zerobase/weather/service/DiaryService.java
+++ weather/src/main/java/zerobase/weather/service/DiaryService.java
@@ -6,6 +6,8 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 import zerobase.weather.domain.Diary;
 import zerobase.weather.repository.DiaryRepository;
 
@@ -19,6 +21,7 @@ import java.util.List;
 import java.util.Map;
 
 @Service
+@Transactional(readOnly = true)
 public class DiaryService {
     @Value("${openweathermap.key}")
     private String apiKey;
@@ -28,6 +31,7 @@ public class DiaryService {
         this.diaryRepository = diaryRepository;
     }
 
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     public void createDiary(LocalDate date, String text) {
         // open weather map 에서 날씨 데이터 가져오기
         String weatherData = getWeatherString();
@@ -42,6 +46,8 @@ public class DiaryService {
         nowDiary.setDate(date);
         diaryRepository.save(nowDiary);
     }
+
+    @Transactional(readOnly = true)
 
     public List<Diary> readDiary(LocalDate date) {
         return diaryRepository.findAllByDate(date);
